bd33d7e5771e1a52668b7aa5bfa4d37d80ee0c03
Melissa Hookey <melissah315@gmail.com>
2023-11-10 18:06:37 -0600

    Revert "commit before delete and start over"
    This reverts commit 7589040dcfa8db7fe6e7bdfabb36e2bc0aa909cd.
    going back
    
    message

--- app.js
+++ app.js
@@ -1,13 +1,7 @@
-// resources used
-// https://developer.spotify.com/documentation/web-api/tutorials/code-pkce-flow
-
 const express = require("express");
 const app = express();
 const port = 8080;
 require("dotenv").config();
-
-var client_id = "CLIENT_ID";
-var client_secret = "CLIENT_SECRET";
 
 app.use("/css", express.static(__dirname + "public/css"));
 app.use("/js", express.static(__dirname + "public/js"));
@@ -16,7 +10,7 @@ app.listen(8080, () => {
   console.log("App is listening on port 8080!");
 });
 
-// takes user to account auth on spotify to allow access
+// takes user to account auth on spotify
 app.get("/", (req, res) => {
   res.send(
     "<a href='https://accounts.spotify.com/authorize?client_id=" +
@@ -27,102 +21,10 @@ app.get("/", (req, res) => {
 
 app.get("/account", async (req, res) => {
   console.log("spotify response code is " + req.query.code);
-  res.send(__dirname + "/account");
+  res.sendFile(__dirname + "/views/index.html");
 });
 
 // after user allows access, redirect to index.html
 app.get("", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
-});
-
-// auth for API
-var redirect_uri = "http://localhost:8888/callback";
-
-app.get("/login", function (req, res) {
-  var state = generateRandomString(16);
-  var scope = "user-read-private user-read-email";
-
-  res.redirect(
-    "https://accounts.spotify.com/authorize?" +
-      querystring.stringify({
-        response_type: "code",
-        client_id: client_id,
-        scope: scope,
-        redirect_uri: redirect_uri,
-        state: state,
-      })
-  );
-});
-
-app.get("/callback", function (req, res) {
-  var code = req.query.code || null;
-  var state = req.query.state || null;
-
-  if (state === null) {
-    res.redirect(
-      "/#" +
-        querystring.stringify({
-          error: "state_mismatch",
-        })
-    );
-  } else {
-    var authOptions = {
-      url: "https://accounts.spotify.com/api/token",
-      form: {
-        code: code,
-        redirect_uri: redirect_uri,
-        grant_type: "authorization_code",
-      },
-      headers: {
-        "content-type": "application/x-www-form-urlencoded",
-        Authorization:
-          "Basic " +
-          new Buffer.from(client_id + ":" + client_secret).toString("base64"),
-      },
-      json: true,
-    };
-  }
-});
-
-// access to user profile info
-async function getProfile() {
-  let accessToken = localStorage.getItem("access_token");
-
-  const response = await fetch("https://api.spotify.com/v1/me", {
-    headers: {
-      Authorization: "Bearer " + accessToken,
-    },
-  });
-
-  const data = await response.json();
-}
-
-// refresh access token
-app.get("/refresh_token", function (req, res) {
-  var refresh_token = req.query.refresh_token;
-  var authOptions = {
-    url: "https://accounts.spotify.com/api/token",
-    headers: {
-      "content-type": "application/x-www-form-urlencoded",
-      Authorization:
-        "Basic " +
-        new Buffer.from(client_id + ":" + client_secret).toString("base64"),
-    },
-    form: {
-      grant_type: "refresh_token",
-      refresh_token: refresh_token,
-    },
-    json: true,
-  };
-
-  request.post(authOptions, function (error, response, body) {
-    if (!error && response.statusCode === 200) {
-      var access_token = body.access_token,
-        refresh_token = body.refresh_token;
-      res.send({
-        access_token: access_token,
-        refresh_token: refresh_token,
-      });
-    }
-  });
 });
